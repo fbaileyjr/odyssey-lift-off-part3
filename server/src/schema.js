61297e00068fe8cad5254db1ffaef2a2901510b3
Frank <frankbaileyjr@gmail.com>
2024-05-30 17:34:06 -0400

    Lesson 3 - GraphQL Arguments

--- server/src/schema.js
+++ server/src/schema.js
@@ -3,7 +3,9 @@ const gql = require('graphql-tag');
 const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
-    tracksForHome: [Track!]!
+    tracksForHome: [Track!]!,
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
