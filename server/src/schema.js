9eb2b2c07a2c632041f104f5fd944ef62d5bb1d7
Frank <frankbaileyjr@gmail.com>
2024-05-30 13:31:25 -0400

    Lesson 2 - Updating our schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -13,12 +13,18 @@ const typeDefs = gql`
     title: String!
     "The track's main Author"
     author: Author!
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
   "Author of a complete Track or a Module"
@@ -28,6 +34,15 @@ const typeDefs = gql`
     name: String!
     "Author's profile picture"
     photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module{
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 `;
 
