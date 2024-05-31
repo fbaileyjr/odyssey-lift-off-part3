c303a58be9fd376263408d5cc09d70bee2620037
Frank <frankbaileyjr@gmail.com>
2024-05-31 11:05:16 -0400

    Lesson 7 - Building the Track Page

---
+++ client/src/pages/track.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+import { useParams } from "react-router-dom";
+
+const Track = () => {
+    const { trackId = "" } = useParams();
+    return <Layout></Layout>;
+};
+
+export default Track;
